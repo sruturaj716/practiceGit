53e6a7d9fd6911a5b596b2b0a5f411b970a1c591
Ruturaj Shinde <sruturaj716@gmail.com>
2023-06-18 20:43:45 +0530

    edit index.js

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ var people = [{ age: 20 }, { age: 30 }, { age: 50 }];
 var players = [{ name: "jon", age: 23 }, { name: "mac", age: 28 }, { name: "dor", age: 90 }];
 
 var person = getOldest(players);
+console.log(person);
